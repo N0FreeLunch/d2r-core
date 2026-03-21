010cffd8db466cf502e5c944839a8e19a8100df6
jin <consilience90@gmail.com>
2026-03-21 12:11:30 +0900

    chore: update data gateway to include item_specs module

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -2,5 +2,6 @@
 pub mod generated;
 
 pub use generated::{
-    item_codes, localization, monsters, property_map, skills, stat_costs,
+    item_codes, item_specs, localization, monsters, property_map, skills,
+    stat_costs,
 };

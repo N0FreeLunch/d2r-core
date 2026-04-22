9525ae67d8abeb47bd19339669f0593625b72729
jin <consilience90@gmail.com>
2026-04-23 01:56:54 +0900

    chore: update source code license headers to PolyForm Noncommercial

--- src/bin/verify/d2bit_diff.rs
+++ src/bin/verify/d2bit_diff.rs
@@ -1,21 +1,8 @@
-﻿// Copyright 2026 N0FreeLunch (https://github.com/N0FreeLunch/d2r-core)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This software is licensed under the PolyForm Noncommercial License 1.0.0.
+// Required Notice: Copyright 2026 N0FreeLunch (https://github.com/N0FreeLunch/d2r-core)
 
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 use std::path::Path;
-use std::process;
 use serde::{Serialize, Deserialize};
 use d2r_core::verify::args::{ArgParser, ArgSpec, ArgError};
 

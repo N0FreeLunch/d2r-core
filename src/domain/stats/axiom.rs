f58110468c9a7a0d7a31057fd0df996a89e0a4de
jin <consilience90@gmail.com>
2026-05-04 17:26:44 +0900

    test(forensic): update axiom metadata test expectations

--- src/domain/stats/axiom.rs
+++ src/domain/stats/axiom.rs
@@ -188,11 +188,11 @@ impl StatsAxiom {
     }
 
     pub fn is_v105_shadow(&self, flags: u32) -> bool {
-        self.save_is_alpha && self.version == 5 && ((flags & (1 << 27)) != 0 || (flags & (1 << 26)) != 0)
+        self.save_is_alpha && (self.version == 5 || self.version == 2) && ((flags & (1 << 27)) != 0 || (flags & (1 << 26)) != 0)
     }
 
     pub fn is_fragment(&self, flags: u32) -> bool {
-        self.save_is_alpha && (self.version == 5 || self.version == 1) && ((flags & (1 << 26)) != 0 || (flags & (1 << 27)) != 0)
+        self.save_is_alpha && (self.version == 5 || self.version == 2 || self.version == 1) && ((flags & (1 << 26)) != 0 || (flags & (1 << 27)) != 0)
     }
 
 
